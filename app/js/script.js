77f74a5cba50d9f4ae47a32ecb0dd1d1eaa3b30b
unknown <ndtuananh04@gmail.com>
2024-08-22 08:53:18 +0700

    second comit

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,26 @@
 const btnHbger = document.querySelector('#btnHbger');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHbger.addEventListener('click', function() {
     console.log('click');
-    if(btnHbger.classList.contains('open')) {
-        btnHbger.classList.remove('open');
-    } else {
-         btnHbger.classList.add('open');
+
+    if(header.classList.contains('open')) { //close menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else { //open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
    
 });
